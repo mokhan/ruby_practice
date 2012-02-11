5e64558fe2827eccb312952aea0539249dfb53f6
mo k <mo@mokhan.ca>
2012-02-11 12:38:10 -0700

    repace the assert_equal syntax with the must_equal syntax.

--- test/book_specs.rb
+++ test/book_specs.rb
@@ -22,13 +22,16 @@ describe Book do
       @book = Book.new("Brain Rules for Baby", "John Medina", @publisher, 2010)
     end
     it "should be equal when it's the same instance" do 
-      assert_equal(@book, @book)
+      @book.must_equal @book
+      #assert_equal(@book, @book)
     end
     it "should be equal when they are the same book" do 
-      assert_equal(@book, Book.new("Brain Rules for Baby", "John Medina", @publisher, 2010))
+      @book.must_equal Book.new("Brain Rules for Baby", "John Medina", @publisher, 2010)
+      #assert_equal(@book, Book.new("Brain Rules for Baby", "John Medina", @publisher, 2010))
     end
     it "should not be equal when they are different books" do 
-      refute_equal(@book, Book.new("Brain Rules for Baby", "some other guy", @publisher, 2009))
+      @book.wont_match Book.new("Brain Rules for Baby", "some other guy", @publisher, 2009)
+      #refute_equal(@book, Book.new("Brain Rules for Baby", "some other guy", @publisher, 2009))
     end
   end
 end
