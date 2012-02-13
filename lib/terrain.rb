ee6968d6fde0072e4bdb1e7b1b047189a6c7157e
mo k <mo@mokhan.ca>
2012-02-13 16:46:57 -0700

    clone the current location

--- lib/terrain.rb
+++ lib/terrain.rb
@@ -4,7 +4,7 @@ class Terrain
   end
 
   def move_forward( heading, location)
-    new_location = heading.forward(location)
+    new_location = heading.forward(location.clone)
     if( new_location[:x] < @map[:x] && new_location[:x] > 0 && new_location[:y] < @map[:y] && new_location[:y] > 0)
       location[:x] = new_location[:x]
       location[:y] = new_location[:y]
